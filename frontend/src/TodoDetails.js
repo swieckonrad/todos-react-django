5f6a9d4ef3ee24bbe5fb852bfde072fbb17b69a7
swieckonrad <68926685+swieckonrad@users.noreply.github.com>
2021-02-01 15:48:45 +0100

    proper details page

--- frontend/src/TodoDetails.js
+++ frontend/src/TodoDetails.js
@@ -1,183 +1,33 @@
 import axios from "axios";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-let render = 1;
-
-// taken from https://usehooks.com/usePrevious/
-function usePrevious(value) {
-  const ref = useRef();
-
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
-
-  return ref.current;
-}
-
-function useEffectAllDepsChange(fn, deps) {
-  const prevDeps = usePrevious(deps);
-  const changeTarget = useRef();
-
-  useEffect(() => {
-    // nothing to compare to yet
-    if (changeTarget.current === undefined) {
-      changeTarget.current = prevDeps;
-    }
-
-    // we're mounting, so call the callback
-    if (changeTarget.current === undefined) {
-      return fn();
-    }
-
-    // make sure every dependency has changed
-    if (changeTarget.current.every((dep, i) => dep !== deps[i])) {
-      changeTarget.current = deps;
-
-      return fn();
-    }
-  }, [fn, prevDeps, deps]);
-}
-
-const KomponentZPaskiemLadowania = () => {
-  const [color, setColor] = useState("red");
-
-  const [innycolor, setinnyColor] = useState("green");
-
-  useEffect(() => {
-    const intervalColor = setInterval(() => {
-      const randomColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16);
-      setColor(randomColor);
-      console.log("Kolor zmieniony na " + randomColor);
-      console.log(innycolor);
-    }, 100);
-    return () => {
-      clearInterval(intervalColor);
-    };
-  }, [innycolor]);
-
-  useEffect(() => {
-    const intervalColor = setInterval(() => {
-      const randomColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16);
-      setinnyColor(randomColor);
-      console.log("inny Kolor zmieniony na " + randomColor);
-    }, 100);
-    return () => {
-      clearInterval(intervalColor);
-    };
-  }, []);
-
-  return (
-    <div
-      style={{
-        backgroundColor: color,
-      }}
-    >
-      Laduje sie...
-    </div>
-  );
-};
 
 export const TodoDetails = () => {
   const { id } = useParams();
-  const [data, setData] = useState();
-  const [counter, setCounter] = useState(0);
+  const [data, setData] = useState([]);
 
-  console.log("renderowanie komponentu...");
-
-  useEffect(() => {
-    // console.clear();
-    console.log("Liczba renderow: " + render);
-    render++;
-
-    console.log(document.querySelector("#moj-button"));
-  });
-
-  useEffect(() => {
-    console.log("wyrenderowano komponent");
-  });
-
-  useEffect(() => {
-    console.log("po raz pierwszy wyrenderowano komponent - początek życia");
-
-    return () => {
-      console.log(
-        "tzw. cleanup function po raz ostatni wyrenderowano komponent - koniec życia"
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/todos/todo/${id}`
       );
-    };
+      setData(response.data);
+    } catch (err) {
+      if (!err.request) {
+        throw err;
+      }
+    }
+  };
+
+  // do useEffect nie można podać funkcji async
+  useEffect(() => {
+    fetchTodos();
   }, []);
-
-  const [daneZSerwera, setDaneZSerwera] = useState("");
-  useEffect(() => {
-    setTimeout(() => {
-      setDaneZSerwera("lista zakupow: monitor, karta graficzna");
-    }, 5000);
-  }, []);
-
-  useEffect(() => {
-    // poczatek zycia
-    const idIntervala = setInterval(() => {
-      console.log("co 1s");
-    }, 1000);
-
-    return () => {
-      // koniec zycia
-      clearInterval(idIntervala);
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log("wyrenderowano komponent po zmianie stanu data");
-  }, [data]);
-
-  useEffect(() => {
-    console.log("wyrenderowano komponent po zmianie stanu counter");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("wyrenderowano komponent po zmianie stanu data lub counter");
-  }, [data, counter]);
-
-  useEffectAllDepsChange(() => {
-    console.log("wyrenderowano komponent po zmianie stanu data i counter");
-  }, [data, counter]);
 
   return (
     <div className="TodoDetails">
-      <button
-        onClick={() => {
-          setData({ text: "bbb" });
-          setCounter((prev) => prev + 1);
-        }}
-      >
-        Dodaj do countera 1 i zmien data
-      </button>
-      <button id="moj-button" onClick={() => setCounter((prev) => prev + 1)}>
-        Dodaj 1 do countera
-      </button>
-      <button onClick={() => setCounter((prev) => prev + 2)}>
-        Dodaj 2 do countera
-      </button>
-      <br />
-      <br />
-      Counter: {counter}
-      <br />
-      <br />
-      {daneZSerwera === "" ? <KomponentZPaskiemLadowania /> : null}
-      {data ? (
-        <div>
-          to jest todo id: {id}
-          <br />
-          text: {data.text}
-        </div>
-      ) : (
-        <div>
-          <div>Loading...</div>;
-          <button onClick={() => setData({ text: "aaa" })}>Zmien data</button>
-        </div>
-      )}
+      {data.text}
+      {data.id}
     </div>
   );
 };
---
+++ frontend/src/UpdateTodo.js
@@ -0,0 +1,128 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Typography,
+  TextField,
+  Button,
+  MenuItem,
+  Container,
+} from "@material-ui/core";
+
+export const UpdateTodo = () => {
+  const [todoTextInput, setTodoText] = useState("");
+  const [todoPriorityUpdate, setPriorityUpdate] = useState();
+  const [todoTextInputError, setTodoTextInputError] = useState("");
+  const [todoPriorityInputError, setTodoPriorityInputError] = useState("");
+
+  const importanceLevel = ["important", "normal", "not important"];
+
+  // const fetchData = () => {
+  //   axios
+  //     .get("http://localhost:8000/todos/todo")
+  //     .then((response) => {
+  //       setTodos(response.data);
+  //     })
+  //     .catch((error) => {
+  //       if (error.request) {
+  //         alert(error.toString());
+  //       } else {
+  //         throw error;
+  //       }
+  //     });
+  // };
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/todos/todo");
+      setTodos(response.data);
+    } catch (err) {
+      if (!err.request) {
+        throw err;
+      }
+    }
+  };
+
+  // do useEffect nie można podać funkcji async
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await axios.get("http://localhost:8000/todos/todo");
+  //     setTodos(response.data);
+  //   })();
+  // }, []);
+
+  const handleAddTodo = () => {
+    axios
+      .post("http://localhost:8000/todos/todo", {
+        text: todoTextInput,
+        priority: todoPriorityUpdate,
+      })
+      .then((response) => {
+        setTodos((prev) => [...prev, response.data]);
+        // alert(`Dodano todo o id: ${response.data.id}`);
+      })
+      .catch((error) => {
+        if (error.response) {
+          if (error.response.data.text) {
+            setTodoTextInputError(error.response.data.text);
+          }
+
+          if (error.response.data.priority) {
+            setTodoPriorityInputError(error.response.data.priority);
+          }
+
+          // alert(
+          //   Object.entries(error.response.data)
+          //     .map((e) => e[0] + ": " + e[1])
+          //     .join("\n")
+          // );
+        }
+        if (!error.request) {
+          throw error;
+        }
+      });
+  };
+
+  // dodawanie, usuwanie, modyfikacja, odswiezanie
+
+  return (
+    <div className="App">
+      <Typography variant="h2">Todos</Typography>
+      <Container>
+        <div className="Container">
+          <TextField
+            variant="outlined"
+            label="Input your todo"
+            fullWidth
+            error={todoTextInputError !== ""}
+            helperText={todoTextInputError}
+            onFocus={() => setTodoTextInputError("")}
+            value={todoTextInput}
+            onChange={(event) => setTodoText(event.target.value)}
+          />
+          <TextField
+            className="Select"
+            select
+            variant="outlined"
+            error={todoPriorityInputError !== ""}
+            helperText={todoPriorityInputError}
+            onFocus={() => setTodoPriorityInputError("")}
+            value={todoPriorityUpdate}
+            onChange={(e) => setPriorityUpdate(e.target.value)}
+          >
+            {importanceLevel.map((imp) => (
+              <MenuItem value={imp}>{imp}</MenuItem>
+            ))}
+          </TextField>
+        </div>
+      </Container>
+      <Button variant="outlined" color="secondary" onClick={handleAddTodo}>
+        Update Todo
+      </Button>
+    </div>
+  );
+};
